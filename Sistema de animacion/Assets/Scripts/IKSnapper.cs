ca9e07cff4af5da08f0cc6e0ccde398597fc3330
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2023-03-28 00:47:29 -0500

    cataño

--- Sistema de animacion/Assets/Scripts/IKSnapper.cs
+++ Sistema de animacion/Assets/Scripts/IKSnapper.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
 public class IKSnapper : MonoBehaviour
 {
-    private float proceduralInfluence;
+    [SerializeField][Range(0,1)] private float proceduralInfluence;
     [SerializeField] private MultiParentConstraint[] animatedBones;
     [SerializeField] private MultiParentConstraint[] proceduralBones;
-    [SerializeField] private AnimationCurve activationAnimation;
-    [SerializeField] private AnimationCurve deactivationAnimation;
-    private bool currentOverride;
-    
+
     private void UpdateInfluence(float weight)
     {
         if (animatedBones == null) return;
@@ -30,25 +25,9 @@ public class IKSnapper : MonoBehaviour
             proceduralConstraint.weight = 1 - weight;
         }
     }
-
-    public void OverrideIK(bool state)
+    
+    private void OnValidate()
     {
-        proceduralInfluence = state ? 1 : 0;
-        if (state != currentOverride)
-        {
-            currentOverride = state;
-            StartCoroutine(AnimateInfluence());
-        }
-    }
-
-    IEnumerator AnimateInfluence()
-    {
-        AnimationCurve curve = currentOverride? activationAnimation: deactivationAnimation;
-        //Actualiza la influencia poco a poco, hasta llegar a los puntos limite
-        for (float time = 0; time < 1f; time += Time.deltaTime)
-        {
-            proceduralInfluence = curve.Evaluate(time);
-            yield return null;
-        }
+        UpdateInfluence(proceduralInfluence);
     }
 }
