f3aa1f1bc19a975c288063d416aaa584eabf46d0
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2023-05-10 21:37:19 -0500

    cambios al cambio

---
+++ Sistema de animacion/Assets/GamesSource/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera Camera;   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
